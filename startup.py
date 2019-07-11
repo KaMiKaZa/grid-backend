7a1b5778724cd67ab6d66734d6ff95ec4a141b9a
KaMiKaZa <kamikaza1997@gmail.com>
2019-07-11 15:11:57 +0500

    registered blueprint hooks

--- startup.py
+++ startup.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from blueprints import database
+from blueprints import database, hooks
 
 
 app = Flask(__name__)
 app.register_blueprint(database.bp)
+app.register_blueprint(hooks.bp)
 
 
 if __name__ == '__main__':
